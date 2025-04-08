827e62854327d8c7c7f402e5ad05fad0ef9a4128
luis <luislemosmsb@gmail.com>
2025-04-08 22:23:18 +0100

    first-component

---
+++ src/app/Helloworld/helloworld.components.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'hello-world',
+  template: '<h1>hello world  : vai dar certo</h1>'
+})
+
+export class HelloworldComponent   {
+
+}
