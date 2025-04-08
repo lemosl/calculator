33b48060dfdbdb37834f13351d7301d1574fdfe4
luis <luislemosmsb@gmail.com>
2025-04-08 22:48:33 +0100

    new CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import  { HelloworldComponent} from './Helloworld/helloworld.components'
+import { MypageComponent } from './mypage/mypage.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HelloworldComponent],
+  imports: [RouterOutlet,HelloworldComponent, MypageComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
